017b3edefc962cb132923dce1788f58da77e7e0f
Liza <elizavetanakasone@gmail.com>
2023-04-16 18:46:11 +0900

    Поправила 17 задачу

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -20,8 +20,8 @@ int Quarter(int x, int y)
 {
     if (x > 0 && y > 0) return 1;
     if (x < 0 && y > 0) return 2;
-    if (x < 0 && y > 0) return 3;
-    if (x > 0 && y > 0) return 4;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
     return 0;
 }
 
