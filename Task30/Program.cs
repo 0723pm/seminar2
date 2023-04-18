97fe081b5d74cb492ce115e115b896e718a4045c
Liza <elizavetanakasone@gmail.com>
2023-04-18 21:24:01 +0900

    Задача 30

---
+++ Task30/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]}");
+//     }
+// }
+
+// Console.WriteLine("Введите длину массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] arr = CreateArray(size);
+// PrintArray(arr);
+
+
+Console.WriteLine("Введите длину массива");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(lenghtArray);
+PrintArray(arr);
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    Random random = new Random(); // оптимизация объекта Random (var - объект неявной типизации)
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
