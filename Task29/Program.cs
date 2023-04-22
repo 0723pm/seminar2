da05eccd10e37b9a40a67238edf36987ff5fcebf
Liza <elizavetanakasone@gmail.com>
2023-04-22 18:54:43 +0900

    29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,3 +3,30 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
+Console.WriteLine("Введите длину массива");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(lenghtArray);
+PrintArray(arr);
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    Random random = new Random(); 
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.WriteLine("]");
+}
