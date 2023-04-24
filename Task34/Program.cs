db88f863bc2877691a1f46544bb4bb506f344a27
Liza <elizavetanakasone@gmail.com>
2023-04-24 22:19:58 +0900

    34

---
+++ Task34/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Запишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+
+
+Console.WriteLine("Введите длину массива");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[lenghtArray];
+
+void CreateArrayRndInt(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,999);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+       Console.Write(arr[i]+ " "); 
+    }
+    
+}
+
+CreateArrayRndInt(arr);
+Console.Write("[");
+PrintArray(arr);
+Console.Write("]");
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+if (arr[i] % 2 == 0)
+count++;
+Console.WriteLine($"В массиве {count} четных чисел");
--- Task37/Program.cs
+++ Task37/Program.cs
@@ -8,40 +8,40 @@
 
 int[] CreateArrayRndInt(int size, int min, int max)
 {
-int[] arr = new int[size];
-Random rnd = new Random();
-for (int i = 0; i < arr.Length; i++)
-{
-arr[i] = rnd.Next(min, max + 1);
-}
-return arr;
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
 }
 
 int[] PairMultipleNumberArray(int[] arr)
 {
-int size = arr.Length / 2;
-if(arr.Length%2 == 1) size+=1;
+    int size = arr.Length / 2;
+    if (arr.Length % 2 == 1) size += 1;
 
 
-int[] newArr = new int [size];
+    int[] newArr = new int[size];
 
 
-for (int i = 0; i < arr.Length/2; i++)
-{
-newArr[i] = arr[i]*arr[arr.Length-1-i];
-}
-if(arr.Length%2 == 1) newArr[newArr.Length-1] = arr[arr.Length/2];
-return newArr;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        newArr[i] = arr[i] * arr[arr.Length - 1 - i];
+    }
+    if (arr.Length % 2 == 1) newArr[newArr.Length - 1] = arr[arr.Length / 2];
+    return newArr;
 
 }
 
 void PrintArray(int[] arr)
 {
-for (int i = 0; i < arr.Length; i++)
-{
-if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-else Console.Write($"{arr[i]}");
-}
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
 }
 
 int[] array = CreateArrayRndInt(4, 1, 10);
